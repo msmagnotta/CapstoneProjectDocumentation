508a3d5f2cfa6684375b4453626e3e0a792d991d
JJ <jeanjacquesakpakoun@gmail.com>
2024-04-22 00:17:18 -0500

    Additional changes

--- server/routes/v2verifyRouter.js
+++ server/routes/v2verifyRouter.js
@@ -1,29 +1,78 @@
 const express = require("express");
 const router = express.Router();
 const axios = require('axios');
+const multer = require('multer');
+const User = require('../modules/User')
+// Set up Multer storage in memory
+const storage = multer.memoryStorage();
+// const FormData = require('form-data');
 
+// Initialize Multer
+const upload = multer({ storage: storage });
+let users = []
 
+async function registerV2Verify (username, gender) {
+  let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: `https://public.v2ondemandapis.com/1/sve/Enrollment/${username}/${gender}`,
+    headers: { 
+      'Cloud-Developer-Key': 'A9676558-C4A8-W647-ACC4-1DE3C5458344', 
+      'Cloud-Application-Key': '7BF2A8B0-79B7-W1CE-A4C3-203008641657', 
+      'Vv-Override-Token': '123456', 
+      'Interaction-Tag': username
+    }
+  };
+  
+  await axios.request(config)
+  .then((response) => {
+    console.log(JSON.stringify(response.data));
+    console.log(JSON.stringify(response.headers));
+    session_id = response.headers['vv-session-id']
+    const user = new User(username, session_id)
+    users.push(user)
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+}
+function SendEnrollmentVoiceData (file, session_id) {
+  let data = new FormData()
+  data.append('data',file)
+  let config = {
+    method: 'post',
+    maxBodyLength: Infinity,
+    url: 'https://public.v2ondemandapis.com/1/sve/Enrollment',
+    headers: { 
+      'Vv-Session-Id': session_id, 
+      ...data.getHeaders()
+    },
+    data : data
+  };
+  
+  axios.request(config)
+  .then((response) => {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch((error) => {
+    // console.log(error);
+  });
+}
 
-router.post('/register', async function (req,res,next) {
-    const {username, gender} = req.body
+router.post('/register',upload.single('file'), async function (req,res,next) {
+  if(!req.file){
+    return res.status(400).json({ message: 'No file uploaded' });
+  }
+if(!req.body){
+  return res.status(400).json({ message: 'No body submitted' });
+
+}
+  const {username, gender} = req.body
+  let session_id = null
     console.log(req.body)
-    let config = {
-        method: 'post',
-        maxBodyLength: Infinity,
-        url: `https://public.v2ondemandapis.com/1/sve/Enrollment/${username}/${gender}`,
-        headers: { 
-          'Cloud-Developer-Key': 'A9676558-C4A8-W647-ACC4-1DE3C5458344', 
-          'Cloud-Application-Key': '7BF2A8B0-79B7-W1CE-A4C3-203008641657', 
-          'Vv-Override-Token': '123456', 
-          'Interaction-Tag': username
-        }
-      };
-      
-      await axios.request(config)
-      .then((response) => {
-        console.log(JSON.stringify(response.data));
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    console.log(req.file)
+    await registerV2Verify(username, gender)
+    await SendEnrollmentVoiceData(req.file, session_id)
 })
+
+module.exports = router
