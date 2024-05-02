fb24bd4d2585909886ceb1e58c1ec2240c93734c
JJ <jeanjacquesakpakoun@gmail.com>
2024-05-01 23:38:55 -0500

    Added variable to toggle kafka on and off

--- server/app.js
+++ server/app.js
@@ -1,56 +1,59 @@
-
-const express = require('express');
-const kafkaService = require('./kafka/kafkaService');
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const {gameRouter} = require('./routes/gameRouter')
-const audioRouter = require('./routes/audioRouter')
-const v2verifyRouter = require('./routes/v2verifyRouter')
+const express = require("express");
+const kafkaService = require("./kafka/kafkaService");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const { gameRouter } = require("./routes/gameRouter");
+const audioRouter = require("./routes/audioRouter");
+const v2verifyRouter = require("./routes/v2verifyRouter");
 const app = express();
-const bodyParser = require('body-parser')
-const jsonParser = bodyParser.json()
-const transcribeText = require('./audio/OpenAI-API')
-const port = 3001
-const cors = require('cors')
-const fs = require('fs');
-
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
+const transcribeText = require("./audio/OpenAI-API");
+const port = 3001;
+const cors = require("cors");
+const fs = require("fs");
+const enableKafka = false;
 
 app.use(jsonParser);
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/game', gameRouter);
-app.use('/audio', audioRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/game", gameRouter);
+app.use("/audio", audioRouter);
 
 // Kafka setup
-// (async () => {
-//     try {
-//         // Connect to Kafka
-//         await kafkaService.connectKafka();
-//         console.log('Kafka connected successfully');
-//         console.log('Kafka consumer running');
-//         //await kafkaService.deleteTopic();
-
-//     } catch (error) {
-//         console.error('Error starting the Kafka service:', error);
-//         process.exit(1);
-//     }
-// })();
+if (enableKafka) {
+  (async () => {
+    try {
+      // Connect to Kafka
+      await kafkaService.connectKafka();
+      console.log("Kafka connected successfully");
+      console.log("Kafka consumer running");
+      //await kafkaService.deleteTopic();
+    } catch (error) {
+      console.error("Error starting the Kafka service:", error);
+      process.exit(1);
+    }
+  })();
+}
 
 // Graceful shutdown
-// process.on('SIGINT', async () => {
-//     console.log('Shutting down server...');
-//     await kafkaService.disconnectKafka();
-//     console.log('Kafka disconnected');
-//     server.close(() => {
-//         console.log('Server stopped');
-//         process.exit(0);
-//     });
-// });
+if (enableKafka) {
+  process.on("SIGINT", async () => {
+    console.log("Shutting down server...");
+    await kafkaService.disconnectKafka();
+    console.log("Kafka disconnected");
+    server.close(() => {
+      console.log("Server stopped");
+      process.exit(0);
+    });
+  });
+}
 
-app.use('/voice', v2verifyRouter)
+app.use("/voice", v2verifyRouter);
 
-
-app.listen(port, () => console.log('Example app is listening on port ' + port +'.' ));
+app.listen(port, () =>
+  console.log("Example app is listening on port " + port + ".")
+);
